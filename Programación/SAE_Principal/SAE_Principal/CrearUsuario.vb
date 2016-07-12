c45ca923a4e502b6da611d29923367b1d18bfde2
XavRuiz <javier.ruizxl@gmail.com>
2016-07-11 20:23:30 -0600

    Configuracion en Clase Crear Usuario

--- Programación/SAE_Principal/SAE_Principal/CrearUsuario.vb
+++ Programación/SAE_Principal/SAE_Principal/CrearUsuario.vb
@@ -17,7 +17,7 @@ Public Class CrearUsuario
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt_crear.Click
 
         Try
-            comando.CommandText = "insert into tbl_sae_proyecto (id_usuario,usuario,contraseña) Values ('" & TextBox1.Text & "','" & TextBox2.Text & "' )"
+            comando.CommandText = "insert into tbl_sae_proyecto (id_usuario,usuario,contraseña) Values ('','" & TextBox1.Text & "','" & TextBox2.Text & "' )"
             comando.ExecuteNonQuery()
             TextBox1.Clear()
             TextBox2.Clear()
@@ -29,5 +29,9 @@ Public Class CrearUsuario
 
     Private Sub CrearUsuario_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         conexion.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
     End Sub
 End Class
