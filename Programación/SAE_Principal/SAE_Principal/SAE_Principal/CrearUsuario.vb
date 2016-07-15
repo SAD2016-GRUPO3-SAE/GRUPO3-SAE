bbb35cf0a22af7968fdcdeda7dfa07a6450d96b5
XavRuiz <javier.ruizxl@gmail.com>
2016-07-15 15:54:26 -0600

    act

--- Programación/SAE_Principal/SAE_Principal/SAE_Principal/CrearUsuario.vb
+++ Programación/SAE_Principal/SAE_Principal/SAE_Principal/CrearUsuario.vb
@@ -31,4 +31,7 @@ Public Class CrearUsuario
         End If
     End Sub
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
