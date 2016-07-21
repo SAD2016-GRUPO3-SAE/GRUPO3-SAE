dfb44e89fe8c334e89e9de0a737f26845b51d0d9
XavRuiz <javier.ruizxl@gmail.com>
2016-07-21 05:00:52 -0600

    Cambios 20160721 0345

--- Programación/SAE_Principal/SAE_Principal/SAE_Principal/CrearCuenta.vb
+++ Programación/SAE_Principal/SAE_Principal/SAE_Principal/CrearCuenta.vb
@@ -3,11 +3,15 @@
 Public Class CrearCuenta
     Dim Dt As DataTable
     Dim Dt2 As DataTable
+    Dim Dt3 As DataTable
     Dim Da As New SqlDataAdapter
     Dim Cmd As New SqlCommand
+    Dim index_socio_actual As Integer
 
     Private Sub CrearCuenta_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        socio_negocio_box.SelectedIndex = -1
+        NoFactura.SelectedIndex = -1
+        index_socio_actual = -10
         Try
             Dim cmd As New SqlCommand("select * from dbo.tbl_sae_socio_negocio", conectar)
             'Dim cas As Integer
@@ -24,7 +28,7 @@ Public Class CrearCuenta
             conectar.Close()
             'MsgBox("Los datos se guardaron con exito")
 
-            Dim cmd2 As New SqlCommand("select * from dbo.tbl_sae_encabezado_factura", conectar)
+            Dim cmd2 As New SqlCommand("select * from dbo.tbl_sae_encabezado_factura where id_socio_negocio = '" & socio_negocio_box.SelectedValue.ToString & "'", conectar)
             conectar.Open()
             Da.SelectCommand = cmd2
             Dt2 = New DataTable
@@ -35,10 +39,13 @@ Public Class CrearCuenta
                 .ValueMember = "id_encabezadof"
             End With
             conectar.Close()
+
+            'saldo_inicial.Text = ("select * from dbo.tbl_sae_encabezado_factura where id_socio_negocio = '" & socio_negocio_box.SelectedValue.ToString & "'", conectar)
+            conectar.Close()
+
         Catch ex As Exception
             NoFactura.Items.Clear()
         End Try
-
 
     End Sub
 
@@ -60,8 +67,9 @@ Public Class CrearCuenta
                 Else
                     MsgBox("No existe el cliente")
                 End If
+
                 Dim cmd As New SqlCommand("insert into tbl_sae_cuentaporcobrar (cpc_identificador, cpc_fecha_inicio, cpc_num_cuotas, cpc_periodo, cpc_fecha_vencimiento, id_socio_negocio) values ('" & identificador.Text & "','" & fecha_inicio & "','" & NoCuotas.Text & "','" & periodo.Text & "','" & fecha_vencimiento & "','" & socio_negocio_box.SelectedValue.ToString & "')", conectar)
-                Dim cmd2 As New SqlCommand(" ")
+                ' Dim cmd2 As New SqlCommand(" ")
                 Dim cas As Integer
                 conectar.Open()
                 cas = cmd.ExecuteNonQuery
@@ -79,7 +87,23 @@ Public Class CrearCuenta
         End If
     End Sub
 
-    Private Sub saldo_inicial_TextChanged(sender As Object, e As EventArgs) Handles saldo_inicial.TextChanged
-
+    Private Sub socio_negocio_box_SelectedIndexChanged(sender As Object, e As EventArgs) Handles socio_negocio_box.SelectedIndexChanged
+        If index_socio_actual = -10 Then
+            index_socio_actual = socio_negocio_box.SelectedIndex
+        ElseIf index_socio_actual <> socio_negocio_box.SelectedIndex Then
+            index_socio_actual = socio_negocio_box.SelectedIndex
+            Dim cmd2 As New SqlCommand("select * from dbo.tbl_sae_encabezado_factura where id_socio_negocio = '" & socio_negocio_box.SelectedValue.ToString & "'", conectar)
+            conectar.Open()
+            Da.SelectCommand = cmd2
+            Dt2 = New DataTable
+            Da.Fill(Dt2)
+            With NoFactura
+                .DataSource = Dt2
+                .DisplayMember = "ecf_nofactura"
+                .ValueMember = "id_encabezadof"
+            End With
+            conectar.Close()
+        End If
     End Sub
+
 End Class
